31bda444130f4dcf5b8d3e9f860bcb421a866b1f
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2025-08-02 12:33:45 +0700

    move main activity to common package

--- app/src/main/kotlin/com/tigasatudesember/bukupemasok/common/presentation/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/tigasatudesember/bukupemasok/common/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.tigasatudesember.bukupemasok.ui.theme
+package com.tigasatudesember.bukupemasok.common.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
